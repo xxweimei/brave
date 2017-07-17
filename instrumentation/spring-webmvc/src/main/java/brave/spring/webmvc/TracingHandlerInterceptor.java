d743b07b66da3deb9efe05a2a1679761da6391d5
shengyang <123456>
2017-07-17 10:38:50 +0800

    添加zipkin黑名单功能

--- instrumentation/spring-webmvc/src/main/java/brave/spring/webmvc/TracingHandlerInterceptor.java
+++ instrumentation/spring-webmvc/src/main/java/brave/spring/webmvc/TracingHandlerInterceptor.java
@@ -8,12 +8,14 @@ import brave.http.HttpServerHandler;
 import brave.http.HttpTracing;
 import brave.propagation.TraceContext;
 import brave.servlet.HttpServletAdapter;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.AsyncHandlerInterceptor;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashSet;
 
 /**
  * Tracing interceptor for Spring Web MVC, which can be used as both an {@link
@@ -21,41 +23,48 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
  */
 public final class TracingHandlerInterceptor extends HandlerInterceptorAdapter {
 
-  public static AsyncHandlerInterceptor create(Tracing tracing) {
-    return new TracingHandlerInterceptor(HttpTracing.create(tracing));
-  }
-
-  public static AsyncHandlerInterceptor create(HttpTracing httpTracing) {
-    return new TracingHandlerInterceptor(httpTracing);
-  }
-
-  final Tracer tracer;
-  final HttpServerHandler<HttpServletRequest, HttpServletResponse> handler;
-  final TraceContext.Extractor<HttpServletRequest> extractor;
-
-  @Autowired TracingHandlerInterceptor(HttpTracing httpTracing) { // internal
-    tracer = httpTracing.tracing().tracer();
-    handler = HttpServerHandler.create(httpTracing, new HttpServletAdapter());
-    extractor = httpTracing.tracing().propagation().extractor(HttpServletRequest::getHeader);
-  }
-
-  @Override
-  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) {
-    if (request.getAttribute(SpanInScope.class.getName()) != null) {
-      return true; // already handled (possibly due to async request)
+    public static AsyncHandlerInterceptor create(Tracing tracing) {
+        return new TracingHandlerInterceptor(HttpTracing.create(tracing));
     }
 
-    Span span = handler.handleReceive(extractor, request);
-    request.setAttribute(SpanInScope.class.getName(), tracer.withSpanInScope(span));
-    return true;
-  }
+    public static AsyncHandlerInterceptor create(HttpTracing httpTracing) {
+        return new TracingHandlerInterceptor(httpTracing);
+    }
 
-  @Override
-  public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
-      Object o, Exception ex) {
-    Span span = tracer.currentSpan();
-    if (span == null) return;
-    ((SpanInScope) request.getAttribute(SpanInScope.class.getName())).close();
-    handler.handleSend(response, ex, span);
-  }
+    final Tracer tracer;
+    final HttpServerHandler<HttpServletRequest, HttpServletResponse> handler;
+    final TraceContext.Extractor<HttpServletRequest> extractor;
+
+    @Autowired
+    private HashSet<String> zipkinBlackList;
+
+    @Autowired
+    TracingHandlerInterceptor(HttpTracing httpTracing) { // internal
+        tracer = httpTracing.tracing().tracer();
+        handler = HttpServerHandler.create(httpTracing, new HttpServletAdapter());
+        extractor = httpTracing.tracing().propagation().extractor(HttpServletRequest::getHeader);
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) {
+        if (this.zipkinBlackList.contains(request.getServletPath())) {
+            return true;
+        }
+        if (request.getAttribute(SpanInScope.class.getName()) != null) {
+            return true; // already handled (possibly due to async request)
+        }
+
+        Span span = handler.handleReceive(extractor, request);
+        request.setAttribute(SpanInScope.class.getName(), tracer.withSpanInScope(span));
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+                                Object o, Exception ex) {
+        Span span = tracer.currentSpan();
+        if (span == null) return;
+        ((SpanInScope) request.getAttribute(SpanInScope.class.getName())).close();
+        handler.handleSend(response, ex, span);
+    }
 }
--- instrumentation/spring-webmvc/src/test/java/brave/spring/webmvc/TracingHandlerInterceptorAutowireTest.java
+++ instrumentation/spring-webmvc/src/test/java/brave/spring/webmvc/TracingHandlerInterceptorAutowireTest.java
@@ -9,27 +9,41 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.util.HashSet;
+
 public class TracingHandlerInterceptorAutowireTest {
 
-  @Configuration static class HttpTracingConfiguration {
-    @Bean HttpTracing httpTracing() {
-      return HttpTracing.create(Tracing.newBuilder().build());
+    @Configuration
+    static class HttpTracingConfiguration {
+        @Bean
+        HttpTracing httpTracing() {
+            return HttpTracing.create(Tracing.newBuilder().build());
+        }
+
+        @Bean
+        HashSet<String> zipkinBlackList() {
+            return new HashSet<String>() {{
+                add("/status");
+            }};
+        }
     }
-  }
 
-  // NOTE: while bean configuration via @Import works with Spring 4, it does not with Spring 3
-  @Configuration @Import(HttpTracingConfiguration.class)
-  static class BeanConfiguration {
-    @Bean HandlerInterceptor tracingInterceptor(HttpTracing httpTracing) {
-      return TracingHandlerInterceptor.create(httpTracing);
+    // NOTE: while bean configuration via @Import works with Spring 4, it does not with Spring 3
+    @Configuration
+    @Import(HttpTracingConfiguration.class)
+    static class BeanConfiguration {
+        @Bean
+        HandlerInterceptor tracingInterceptor(HttpTracing httpTracing) {
+            return TracingHandlerInterceptor.create(httpTracing);
+        }
     }
-  }
 
-  @Test public void autowiredWithBeanConfig() {
-    AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-    ctx.register(BeanConfiguration.class);
-    ctx.refresh();
+    @Test
+    public void autowiredWithBeanConfig() {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(BeanConfiguration.class);
+        ctx.refresh();
 
-    ctx.getBean(HandlerInterceptor.class);
-  }
+        ctx.getBean(HandlerInterceptor.class);
+    }
 }
